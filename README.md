7cef342d692cb491b567aae4989aa3b3b1fbe656
hosne mubarak <h.m.tasmir.cse@gmail.com>
2022-01-17 18:25:11 +0600

    fix bd setting

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 ## Django Weather Project
 
+## Configure database
+
+NAME': 'weatherDB',
+'USER': 'postgres',
+'PASSWORD': 'tasmir',
+
+active virtualenv
+
 pip install -r requirements.txt
 
 `Step 1: python manage.py makemigration`</br>
-`step 2: 
-    python manage.py migrate`</br>
+`step 2:
+python manage.py migrate`</br>
 `step 3:
-    python manage.py runserver 8000`</br>
+python manage.py runserver 8000`</br>
 `step 4:
-    # in root directory you can search weather of city and can perform crud after saving data.
-    # When we are save data than we are saveing city name using signal.`</br>
+
+# in root directory you can search weather of city and can perform crud after saving data. # When we are save data than we are saveing city name using signal.`</br>
 
 ## Endpoints
+
 all endpoints
 http://127.0.0.1:8000/api/
     
--- weather_api_project/settings.py
+++ weather_api_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,6 +23,7 @@ SECRET_KEY = 'django-insecure-k+m7pecxmnard&@%n7i2x20^)-d40s9k7lh1b_1&iur(fry#fw
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -81,10 +82,21 @@ WSGI_APPLICATION = 'weather_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weatherDB',
+        'USER': 'postgres',
+        'PASSWORD': 'tasmir',
     }
 }
 
